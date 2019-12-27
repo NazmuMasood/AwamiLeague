d3993e251e72016f4abcb3fdd687ba13af0f3728
Nazmuddin Al Masood <nazmumasood96@gmail.com>
2019-12-27 15:38:27 +0600

    code optimized

--- app/src/main/java/com/nazmu/awamileague/MainActivity.java
+++ app/src/main/java/com/nazmu/awamileague/MainActivity.java
@@ -7,10 +7,8 @@ import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
@@ -30,7 +28,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
@@ -38,19 +35,9 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final int PERMISSION_ALL = 1;
-    String[] PERMISSIONS = {
-            android.Manifest.permission.RECORD_AUDIO,
-            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            android.Manifest.permission.CAMERA,
-            Manifest.permission.MODIFY_AUDIO_SETTINGS
-    };
-
     ProgressBar progressBar; ImageView progressBarIV;
     WebView webView;
     SwipeRefreshLayout swipeLayout;
-    //String BANGLA_FONT_SOLAIMAN_LIPI = "fonts/SolaimanLipi_22-02-2012.ttf";
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     CircleFillView circleFill;
@@ -68,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
         swipeLayout = findViewById(R.id.swipeLayout);
         drawerLayout = findViewById(R.id.drawerLayout);
         navigationView = findViewById(R.id.nav_view);
-        circleFill = (CircleFillView) findViewById(R.id.circleFillView);
+        circleFill = findViewById(R.id.circleFillView);
         toolbar = findViewById(R.id.toolbar);
 
         //Setting up the toolbar with a custom app-title
@@ -80,8 +67,6 @@ public class MainActivity extends AppCompatActivity {
         //Loading the web-page url
         webView.loadUrl("https://www.albd.com.bd/");
         webView.getSettings().setJavaScriptEnabled(true);
-        /*webView.getSettings().setAllowFileAccessFromFileURLs(true);
-        webView.getSettings().setAllowUniversalAccessFromFileURLs(true);*/
         webView.getSettings().setDomStorageEnabled(true);
         webView.getSettings().setAllowContentAccess(true);
         webView.getSettings().setAllowFileAccess(true);
@@ -109,7 +94,6 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onAnimationEnd(Animator animation) {
                         super.onAnimationEnd(animation);
-                        // start your activity here
                         progressBar.setVisibility(View.GONE);
                         circleFill.setVisibility(View.GONE);
                         progressBarIV.setVisibility(View.GONE);
@@ -121,66 +105,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
-                //progressBar.setVisibility(View.GONE);
-                //circleFill.setVisibility(View.GONE);
-                //progressBarIV.setVisibility(View.GONE);
                 swipeLayout.setRefreshing(false);
             }
         });
 
-        /*webView.setWebChromeClient(new WebChromeClient(){
-            @Override
-            public void onPermissionRequest(PermissionRequest request) {
-                //super.onPermissionRequest(request);
-                //Needs to accept permission to access the camera
-                //if (checkPermissions()>=3) {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        if (!hasPermissions(MainActivity.this, PERMISSIONS)) {
-                            ActivityCompat.requestPermissions(MainActivity.this, PERMISSIONS, PERMISSION_ALL);
-                        }
-                        else {
-                            request.grant(request.getResources());
-                        }
-                    }
-                    else {
-                        request.grant(request.getResources());
-                    }
-                //}
-            }
-
-            @Override
-            public void onProgressChanged(WebView view, int newProgress) {
-                super.onProgressChanged(view, newProgress);
-                progressBar.setProgress(newProgress);
-                circleFill.setValue(newProgress);
-
-
-            }
-        });*/
-
         webView.setWebChromeClient(new MyWebChromeClient(){
             @Override
             public void onPermissionRequest(PermissionRequest request) {
-                //super.onPermissionRequest(request);
-                //Needs to accept permission to access the camera
-                /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (!hasPermissions(MainActivity.this, PERMISSIONS)) {
-                        ActivityCompat.requestPermissions(MainActivity.this, PERMISSIONS, PERMISSION_ALL);
-                    }
-                    else {
-                        request.grant(request.getResources());
-                    }
-                }
-                else {
-                    request.grant(request.getResources());
-                }*/
+                super.onPermissionRequest(request);
             }
 
             @Override
             public void onProgressChanged(WebView view, int newProgress) {
                 super.onProgressChanged(view, newProgress);
-                //progressBar.setProgress(newProgress);
-                //circleFill.setValue(newProgress);
             }
         });
 
@@ -190,9 +127,6 @@ public class MainActivity extends AppCompatActivity {
                 webView.reload();
             }
         });
-
-        //Setting up bangla font
-        //Typeface tf = Typeface.createFromAsset(getAssets(),BANGLA_FONT_SOLAIMAN_LIPI);
 
         //Setting up custom toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -227,12 +161,6 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
-        //checking permissions in the beginning
-        /*if (!hasPermissions(MainActivity.this, PERMISSIONS)) {
-            ActivityCompat.requestPermissions(MainActivity.this, PERMISSIONS, PERMISSION_ALL);
-        }*/
-
     }
 
     @Override
@@ -262,60 +190,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /*public static boolean hasPermissions(Context context, String... permissions) {
-        if (context != null && permissions != null) {
-            for (String permission : permissions) {
-                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }*/
-
-    /*@Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case PERMISSION_ALL:{
-                if (grantResults.length > 0) {
-                    String permissionsDenied = "";
-                    for (String per : permissions) {
-                        if(grantResults[0] == PackageManager.PERMISSION_DENIED){
-                            permissionsDenied += "\n" + per;
-
-                        }
-
-                    }
-                    // Show permissionsDenied
-                    if (!permissionsDenied.equals("")){
-                        Toast.makeText(this, "Please accept all the permissions to proceed further",
-                                Toast.LENGTH_LONG).show();
-                    }
-                }
-                return;
-            }
-        }
-    }*/
-
-
-
     public ValueCallback<Uri[]> uploadMessage;
     private ValueCallback<Uri> mUploadMessage;
     public static final int REQUEST_SELECT_FILE = 100;
     private final static int FILECHOOSER_RESULTCODE = 1000;
     class MyWebChromeClient extends WebChromeClient {
-        // For 3.0+ Devices (Start)
-        // onActivityResult attached before constructor
-        /*protected void openFileChooser(ValueCallback uploadMsg, String acceptType) {
-            mUploadMessage = uploadMsg;
-            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-            i.addCategory(Intent.CATEGORY_OPENABLE);
-            i.setType("image/*");
-            startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
-        }*/
-
-
         // For Lollipop 5.0+ Devices
         @TargetApi(Build.VERSION_CODES.LOLLIPOP)
         public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
@@ -337,22 +216,6 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
-        //For Android 4.1 only
-        /*protected void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
-            mUploadMessage = uploadMsg;
-            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-            intent.addCategory(Intent.CATEGORY_OPENABLE);
-            intent.setType("image/*");
-            startActivityForResult(Intent.createChooser(intent, "File Chooser"), FILECHOOSER_RESULTCODE);
-        }*/
-
-        /*protected void openFileChooser(ValueCallback<Uri> uploadMsg) {
-            mUploadMessage = uploadMsg;
-            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-            i.addCategory(Intent.CATEGORY_OPENABLE);
-            i.setType("image/*");
-            startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
-        }*/
     }
 
     @Override
@@ -368,8 +231,6 @@ public class MainActivity extends AppCompatActivity {
         } else if (requestCode == FILECHOOSER_RESULTCODE) {
             if (null == mUploadMessage)
                 return;
-            // Use MainActivity.RESULT_OK if you're implementing WebView inside Fragment
-            // Use RESULT_OK only if you're implementing WebView inside an Activity
             Uri result = intent == null || resultCode != MainActivity.RESULT_OK ? null : intent.getData();
             mUploadMessage.onReceiveValue(result);
             mUploadMessage = null;
